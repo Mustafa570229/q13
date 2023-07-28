8af9ce37e4b2032342cd70cd02f9f48209807c6f
Your Name <mustafaalabohasne@gmail.com>
2023-07-29 01:46:13 +0300

    metadata update for danamic

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,13 +1,18 @@
 
 import ShowingData from "../component/ShowingData";
 
-
-export const metadata = {
-  title: 'Read ore in Kyoo',
-  description: 'In a world filled with questions and doubts, it is essential to seek answers that can strengthen our faith and provide us with certainty.',
-  keywords :["islam, muslims, koran ,muslim prayer,hadith,nikah,evolution in islam"],
-  icons: {
-    icon: '/iconIslam.svg',
+export async function generateMetadata(props) {
+  const data = await getData();
+  const filteredData = await data?.filter(el => el.newCollection.replace(/\s/g, "-").toLowerCase() ===
+    props.params.id.toLowerCase())
+    console.log(filteredData[0].newTitle)
+  return {
+    title: filteredData[0].newTitle,
+    description: filteredData[0].newContent,
+    keywords: ["islam, muslims, koran ,muslim prayer,hadith,nikah,evolution in islam"],
+    icons: {
+      icon: '/iconIslam.svg',
+    }
   }
 }
 
@@ -23,8 +28,8 @@ export default async function Page(props) {
 };
 export async function getData() {
   const res = await fetch('https://kyooadmin.vercel.app/api/mydata',
-  { next: { revalidate: 10 } }
-  
+    { next: { revalidate: 10 } }
+
   );
 
   if (!res.ok) {
