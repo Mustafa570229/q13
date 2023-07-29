89e8c80f93f45aa95a2a60f2f9320351f18d4983
Your Name <mustafaalabohasne@gmail.com>
2023-07-29 05:22:09 +0300

    ads scripte add

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -5,10 +5,9 @@ export async function generateMetadata(props) {
   const data = await getData();
   const filteredData = await data?.filter(el => el.newCollection.replace(/\s/g, "-").toLowerCase() ===
     props.params.id.toLowerCase())
-    console.log(filteredData[0].newTitle)
   return {
-    title: filteredData[0].newTitle,
-    description: filteredData[0].newContent,
+    title: filteredData[0]?.newTitle,
+    description: filteredData[0]?.newContent,
     keywords: ["islam, muslims, koran ,muslim prayer,hadith,nikah,evolution in islam"],
     icons: {
       icon: '/iconIslam.svg',
--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const metadata = {
   title: 'kyoo page',
   description: 'The series begins by addressing fundamental questions that often arise in the minds of Muslims and non-Muslims alike.Questions such as Why am I a Muslim? and Is Islam the religion of truth?',
-  keywords :["islam, muslim, keyword3"],
+  keywords: ["islam, muslim, keyword3"],
   icons: {
     icon: '/iconIslam.svg',
   }
@@ -16,6 +16,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <body className="">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8531563006408715"
+          crossorigin="anonymous"></script>
         <MainNav />
         <div>{children}</div>
       </body>
